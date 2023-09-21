9961f8f7dca9667d9ae5fd3f9d4ffe492014717e
RAJALAKSHM22 <rajalakshmiasokan500@gmail.com>
2023-09-21 10:12:52 +0530

    ADDING TITLE IN CONSOLE

--- json.js
+++ json.js
@@ -1,3 +1,31 @@
-let datum=[{'family':{'father':{'name':'Numjun','age':['30'],'hobby':['vlog making','Image editing'],'income':['1.5L/m']},'mother':{ 'name':'Camila Cabello','age':['27'],'hobby':['vlog making','Image editing','cooking'],'income':['1.5L/m'] },'son':{ 'name':'Lee Dong','age':['6'],'hobby':['playing'],'income':['0'] },'daugther':{'name':'Shin Hye','age':['5'],'hobby':['coloring'],'income':['0']}}},{'Office':{'CEO':{'name':'Kim','age':['58'],'YoJ':['2007'],'income':['4.5L/m']},'President':{'name':'jin','age':['33'],'YoJ':['2015'],'income':['10.5L/m'] },'chairPerson':{'name':'Lee Sung','age':['36'],'YoJ':['2012'],'income':['15.8L/m']}}}]
 
-console.log(datum);
+console.log("PROGRAME OF FOR LOOPING!! ")
+
+let datum=[{'family':{'father':{'Name':'Numjun','age':['30'],'hobby':['vlog making','Image editing'],'income':['1.5L/m']},'mother':{ 'Name':'Camila Cabello','age':['27'],'hobby':['vlog making','Image editing','cooking'],'income':['1.5L/m'] },'son':{ 'Name':'Lee Dong','age':['6'],'hobby':['playing'],'income':['0'] },'daugther':{'Name':'Shin Hye','age':['5'],'hobby':['coloring'],'income':['0']}}},{'Office':{'CEO':{'Name':'Kim','age':['58'],'YoJ':['2007'],'income':['4.5L/m']},'President':{'Name':'Jin','age':['33'],'YoJ':['2015'],'income':['10.5L/m'] },'chairPerson':{'Name':'Lee Sung','age':['36'],'YoJ':['2012'],'income':['15.8L/m']}}}]
+
+console.log("FOR LOOP");    
+// for Loop
+
+for(let i=0;i<datum.length;i++){
+    
+    console.log(datum[i]);
+}
+
+console.log("FOR-IN LOOP");
+// for-in Loop
+
+for(let k in datum){
+    console.log(k,datum[k]);
+}
+
+console.log("FOR-OF LOOP");
+// for-of Loop
+
+for(let j of datum){
+    console.log(j);
+}
+
+console.log("FOR-EACH LOOP" );
+// for-each Loop
+
+datum.forEach((Data)=>console.log(Data));
